06ec407d2f5beaea4b02fbe7bc61607eb7766b8f
Rodrigo Castro <rodrigocd28@gmail.com>
2025-03-18 20:16:54 -0300

    santander dev week

---
+++ src/main/java/me/dio/SantanderDevWek2025Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWek2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWek2025Application.class, args);
+	}
+
+}
